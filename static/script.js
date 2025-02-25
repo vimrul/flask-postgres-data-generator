c2d59d6bf2517ea3ab6ec1e5f947907aa7751cb8
vimrul <viimrul@gmail.com>
2025-02-25 13:38:25 +0600

    added styles

---
+++ static/script.js
@@ -0,0 +1,45 @@
+function connectDB() {
+    let data = {
+        host: document.getElementById("host").value,
+        port: document.getElementById("port").value,
+        dbname: document.getElementById("dbname").value,
+        user: document.getElementById("user").value,
+        password: document.getElementById("password").value
+    };
+
+    fetch("/connect_db", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+      .then(data => alert(data.status === "success" ? "Connected successfully!" : `Error: ${data.message}`));
+}
+
+function startGeneration() {
+    fetch("/start_generation", { method: "POST" })
+        .then(response => response.json())
+        .then(data => alert("Data generation started!"));
+}
+
+function stopGeneration() {
+    fetch("/stop_generation", { method: "POST" })
+        .then(response => response.json())
+        .then(data => alert("Data generation stopped!"));
+}
+
+function fetchData() {
+    fetch("/fetch_data")
+        .then(response => response.json())
+        .then(data => {
+            let display = "<h3>Customers</h3>";
+            data.customers.forEach(c => display += `<p>${c[1]} - ${c[2]} - ${c[3]}</p>`);
+
+            display += "<h3>Transactions</h3>";
+            data.transactions.forEach(t => display += `<p>Customer ID: ${t[1]} - Amount: ${t[2]}</p>`);
+
+            display += "<h3>Orders</h3>";
+            data.orders.forEach(o => display += `<p>Customer ID: ${o[1]} - Product: ${o[2]} - Quantity: ${o[3]}</p>`);
+
+            document.getElementById("data-display").innerHTML = display;
+        });
+}
